99cc951d797a7efa37f91ea99e90bae51fd31724
rakkagaming <robin.lennart.johnsson@gmail.com>
2016-04-06 14:02:30 +0200

    Done some things in controller

--- src/main/java/org/demo/controller/TimeController.java
+++ src/main/java/org/demo/controller/TimeController.java
@@ -1,39 +1,37 @@
 package org.demo.controller;
 
+import org.demo.model.TimeSamples;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Timestamp;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by Anton on 2016-04-05.
  */
 @RestController
-@RequestMapping("/time")
+@RequestMapping("/api/time")
 public class TimeController {
-
-    @Autowired
-    private TimeRepository timeRepository;
+    private ArrayList<TimeSamples> timeStamps;
 
     @RequestMapping(method = RequestMethod.POST)
     public Map<String, Object> timeStamps(@RequestBody Map <String, Object> timeMap){
-        TimeStamps timestamp = new TimeStamps(Integer.parseInt(timeMap.get("day").toString()), Integer.parseInt(timeMap.get("hour").toString()), Integer.parseInt(timeMap.get("minute").toString()));
+
+        TimeSamples timestamp = new TimeSamples();
         Map<String, Object> response = new LinkedHashMap<String, Object>();
         response.put("message", "TimeStamp created succesfully");
 
-        response.put("Timestamp", timeRepository.save(timestamp));
+        response.put("Timestamp", timeStamps.add(timestamp));
 
         return response;
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "/{id}")
     public Map<String, String> deleteTimeStamp(@PathVariable("id") String id) {
-        timeRepository.delete(id);
+        timeStamps.remove(id);
         Map<String, String> response = new HashMap<>();
         response.put("message", "Timestamp deleted successfully");
 
@@ -42,24 +40,25 @@ public class TimeController {
 
     @RequestMapping(method = RequestMethod.GET)
     public Map<String, Object> getAllTimeStamps() {
-        List<TimeStamps> timestamps = timeRepository.findAll();
         Map<String, Object> response = new LinkedHashMap<>();
-        response.put("totalTimestamps", timestamps.size());
-        response.put("UserID", timestamps);
+        response.put("totalTimestamps", timeStamps.size());
+        response.put("UserID", timeStamps);
         return response;
     }
-    @RequestMapping("/time")
-    public ResponseEntity<TimeStamps> getTimeStamp(@PathVariable("id")  String id) {
-        Timestamp timestamp = timeRepository.findOne(id);
+    @RequestMapping("/time&{id}")
+    public ResponseEntity<TimeSamples> getTimeStamp(@PathVariable("id")  String id) {
+//        for(int i=0; i<timeStamps.size(); i++){
+//            if(timeStamps.get(i).getUserId().equals(id)){
+//
+//            }
+//        }
+        TimeSamples timestamp = timeStamps.get(Integer.parseInt(id));
 
         if(timestamp == null) {
-            return new ResponseEntity<TimeStamps>(HttpStatus.NOT_FOUND);
+            return new ResponseEntity<TimeSamples>(HttpStatus.NOT_FOUND);
         }
 
-        return new ResponseEntity<TimeStamps>(timestamp, HttpStatus.OK);
-
-
-
+        return new ResponseEntity<TimeSamples>(timestamp, HttpStatus.OK);
     }
 
 
