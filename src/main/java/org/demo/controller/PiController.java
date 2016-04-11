d9a9c689b50644407e2f5d20d849b41d62915331
sebhero <trevligt.att.ses@gmail.com>
2016-04-11 13:52:30 +0200

    lagt till PiService

---
+++ src/main/java/org/demo/controller/PiController.java
@@ -0,0 +1,36 @@
+package org.demo.controller;
+
+import org.demo.model.PiStamp;
+import org.demo.model.RfidKey;
+import org.demo.service.PiService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by seb on 2016-04-07.
+ */
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/pi")
+public class PiController {
+
+
+	@Autowired
+	private PiService piService;
+
+
+	/***
+	 * Handle rest request from client
+	 * @param rfidKey
+	 * @return
+	 */
+	@RequestMapping(method = RequestMethod.GET, value = "/{id}")
+	public ResponseEntity<PiStamp> timeStampUser(@PathVariable("id") RfidKey rfidKey) {
+
+		return piService.addNewStamp(rfidKey);
+
+	}
+
+}
---
+++ src/main/java/org/demo/service/PiService.java
@@ -0,0 +1,26 @@
+package org.demo.service;
+
+import org.demo.model.PiStamp;
+import org.demo.model.RfidKey;
+import org.demo.model.TimeStamp;
+import org.demo.model.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+/**
+ * Created by seb on 2016-04-11.
+ */
+
+@Service
+public interface PiService {
+
+	/**
+	 * Handles adding new stamp from Pi
+	 * @return
+	 */
+	public ResponseEntity<PiStamp> addNewStamp(RfidKey rfidKey);
+
+}
